d1bb975f87d188fbf985818789f17cbdca06416b
ilyaizr <ilyaizrailyan@gmail.com>
2022-05-05 15:41:26 +0400

    review fixes #1

--- src/utils/markdown/parsers.ts
+++ src/utils/markdown/parsers.ts
@@ -1,11 +1,12 @@
 export function parseLinksAsTags(text?: string): string {
   if (!text) return '';
   let parsed = text;
-  // this regex will find all the links, both in standart and .md format. '.md' format is [label](link)
-  // with 2 extra characters at the begining (to find .md links)
-  // See regex explanation at https://regex101.com/
   // In regex we cannot use negative lookbehind because of Safari:(
-  // Therefore i need to implement following logic using .startsWith()
+  // Therefore i need to implement following logic:
+  // this regex will find all the links, both in standart and .md format. '.md' format is [label](link)
+  // with 2 extra characters at the begining (to throw away links in .md format)
+
+  // See regex explanation at https://regex101.com/
   const regex =
     /(.?.?https|.?.?http)(:\/\/)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/gm;
   const matchIterable = text.matchAll(regex);
@@ -19,20 +20,20 @@ export function parseLinksAsTags(text?: string): string {
     // skip links in propper markdown format
     if (match.startsWith('](')) return;
     // get full link to work with it
-    const linkIndex = match.indexOf('http');
-    const fullLink = match.substring(linkIndex);
-    const linkPrefix = match.substring(0, linkIndex);
+    const linkStartIndex = match.indexOf('http');
+    const fullLink = match.substring(linkStartIndex);
+    const beforeLink = match.substring(0, linkStartIndex);
 
     // we're not using replace methods because they might not work for the same repeating links
     const finalText =
       parsed.substring(0, matchIndex + lengthIncreased) +
-      linkPrefix +
+      beforeLink +
       `[${domain}${path ?? ''}](${fullLink})` +
       parsed.substring(matchIndex + matchLength + lengthIncreased);
 
     lengthIncreased +=
       `[${domain}${path ?? ''}](${fullLink})`.length -
-      (matchLength - linkPrefix.length);
+      (matchLength - beforeLink.length);
     parsed = finalText;
   });
 
